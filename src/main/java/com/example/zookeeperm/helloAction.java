4675fefcbd8f6b238e02b5c252719dcaa12c0bb3
niu <1570012077@qq.com>
2023-03-24 11:33:32 +0800

    开始

---
+++ src/main/java/com/example/zookeeperm/helloAction.java
@@ -0,0 +1,13 @@
+package com.example.zookeeperm;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class helloAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+
+    }
+}
