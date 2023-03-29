293daafc9d13e9170d5b33afca18067ea87a19d6
nss <1570012077@qq.com>
2023-03-29 22:55:52 +0800

    修改组件

--- src/main/java/com/example/zookeeperm/action/ListWindowFactory.java
+++ src/main/java/com/example/zookeeperm/action/ListWindowFactory.java
@@ -1,5 +1,6 @@
 package com.example.zookeeperm.action;
 
+import com.example.zookeeperm.gui.OperationWindow;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.openapi.wm.ToolWindow;
@@ -8,12 +9,19 @@ import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+
 public class ListWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         ContentFactory instance = ContentFactory.SERVICE.getInstance();
         SimpleToolWindowPanel panel = new SimpleToolWindowPanel(false,true);
         Content content = instance.createContent(panel, "", false);
+
+        OperationWindow operationWindow = new OperationWindow(toolWindow);
+        JPanel contentPanel = operationWindow.getContentPanel();
+        panel.setContent(contentPanel);
+
         toolWindow.getContentManager().addContent(content);
     }
 }
--- src/main/java/com/example/zookeeperm/gui/OperationWindow.java
+++ src/main/java/com/example/zookeeperm/gui/OperationWindow.java
@@ -1,7 +1,14 @@
 package com.example.zookeeperm.gui;
 
-import javax.swing.*;
+import com.intellij.ide.ui.laf.darcula.ui.DarculaSplitPaneUI;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.components.JBPanel;
+import com.intellij.ui.components.JBScrollPane;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicSplitPaneDivider;
+import javax.swing.plaf.basic.BasicSplitPaneUI;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 public class OperationWindow {
     private JPanel contentPanel;
@@ -9,8 +16,28 @@ public class OperationWindow {
     private JScrollPane leftPane;
     private JPanel rightPane;
     private JTree fieldTree;
-
     public void init(ToolWindow toolWindow) {
-        fieldTree.setCellRenderer(new TreeCell());
+        contentPanel = new JBPanel<>();
+        leftPane = new JBScrollPane();
+        rightPane = new JPanel();
+        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPane, rightPane);
+        splitPane.setUI(new DarculaSplitPaneUI());
+        contentPanel.add(splitPane);
+
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("root");
+        DefaultMutableTreeNode child1 = new DefaultMutableTreeNode("child1");
+        DefaultMutableTreeNode child2 = new DefaultMutableTreeNode("child2");
+        root.add(child1);
+        root.add(child2);
+        fieldTree = new JTree(root);
+        leftPane.setViewportView(fieldTree);
+    }
+
+    public OperationWindow(ToolWindow toolWindow) {
+        init(toolWindow);
+    }
+
+    public JPanel getContentPanel() {
+        return contentPanel;
     }
 }
