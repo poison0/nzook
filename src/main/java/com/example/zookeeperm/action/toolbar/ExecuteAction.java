c8d29f6ba9dea8f0e80429d1fc0480ed7a277f65
nss <1570012077@qq.com>
2023-04-02 22:39:54 +0800

    添加更新按钮可用性

--- src/main/java/com/example/zookeeperm/action/toolbar/ExecuteAction.java
+++ src/main/java/com/example/zookeeperm/action/toolbar/ExecuteAction.java
@@ -5,12 +5,17 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class ExecuteAction extends AnAction {
+public class ExecuteAction extends AbstractAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
         //todo
     }
     public ExecuteAction() {
         super("Connection", "Connection", AllIcons.Actions.Execute);
+    }
+
+    @Override
+    protected boolean isEnabled(AnActionEvent e) {
+        return false;
     }
 }
--- src/main/java/com/example/zookeeperm/action/toolbar/SuspendAction.java
+++ src/main/java/com/example/zookeeperm/action/toolbar/SuspendAction.java
@@ -3,16 +3,25 @@ package com.example.zookeeperm.action.toolbar;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.editor.Editor;
 import org.jetbrains.annotations.NotNull;
 
-public class SuspendAction extends AnAction {
+public class SuspendAction extends AbstractAction {
 
     public SuspendAction() {
         super("Stop", "Stop", AllIcons.Actions.Suspend);
     }
 
     @Override
-    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        //todo
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+
+    // 该方法用于设置Action的可用性
+    @Override
+    protected boolean isEnabled(AnActionEvent e) {
+        return false;
     }
 }
