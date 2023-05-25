6b9879993954900d8eed68281289f0d12cbfdfe4
niu <1570012077@qq.com>
2023-05-25 18:32:25 +0800

    添加弹框

--- src/main/java/com/example/zookeeperm/action/menu/AddAction.java
+++ src/main/java/com/example/zookeeperm/action/menu/AddAction.java
@@ -1,5 +1,6 @@
 package com.example.zookeeperm.action.menu;
 
+import com.example.zookeeperm.gui.pop.UpdateNode;
 import com.example.zookeeperm.util.Bundle;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
@@ -11,7 +12,10 @@ import org.jetbrains.annotations.NotNull;
 public class AddAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-
+        if (new UpdateNode().showAndGet()) {
+            // user pressed OK
+            System.out.println("OK");
+        }
     }
     public AddAction() {
         super(Bundle.getString("action.AddAction.text"), Bundle.getString("action.AddAction.description"), AllIcons.General.Add);
--- src/main/java/com/example/zookeeperm/gui/pop/UpdateNode.java
+++ src/main/java/com/example/zookeeperm/gui/pop/UpdateNode.java
@@ -1,8 +1,45 @@
-package com.example.zookeeperm.gui.pop;/**
- * @author nss
- * @description 
- * @date 2023/5/24
- * @copyright 广州瑞云网络科技有限公司
+package com.example.zookeeperm.gui.pop;
+
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.ui.OnePixelSplitter;
+import com.intellij.ui.border.CustomLineBorder;
+import com.intellij.util.ui.JBUI;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author niu
  */
-public class UpdateNode {
+public class UpdateNode extends DialogWrapper {
+
+    public UpdateNode() {
+        super(true);
+        setTitle("New Node");
+        init();
+    }
+
+    @Override
+    protected @Nullable JComponent createCenterPanel() {
+        OnePixelSplitter onePixelSplitter = new OnePixelSplitter(false, 0.4f);
+        JPanel dialogPanel = new JPanel(new BorderLayout());
+        dialogPanel.add(onePixelSplitter, BorderLayout.CENTER);
+
+        JPanel left = new JPanel(new BorderLayout());
+        JLabel label = new JLabel("left");
+        left.add(label, BorderLayout.CENTER);
+
+        onePixelSplitter.setFirstComponent(left);
+
+        JPanel reght = new JPanel(new BorderLayout());
+        JLabel rightLabel = new JLabel("rightLabel");
+        reght.add(rightLabel, BorderLayout.CENTER);
+
+        onePixelSplitter.setSecondComponent(reght);
+
+        dialogPanel.setPreferredSize(new Dimension(400, 200));
+
+        return dialogPanel;
+    }
 }
