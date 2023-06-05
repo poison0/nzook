6b0a85287ffcbd1930586e30485410b8436d7df9
nss <1570012077@qq.com>
2023-06-05 23:31:14 +0800

    添加删除一个节点的方法

--- src/main/java/com/example/zookeeperm/action/menu/DeleteAction.java
+++ src/main/java/com/example/zookeeperm/action/menu/DeleteAction.java
@@ -2,18 +2,16 @@ package com.example.zookeeperm.action.menu;
 
 import com.example.zookeeperm.gui.OperationWindow;
 import com.example.zookeeperm.gui.pop.ConfirmDialog;
+import com.example.zookeeperm.message.Notifier;
 import com.example.zookeeperm.util.Bundle;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.Project;
-import com.intellij.ui.treeStructure.SimpleNode;
+import com.intellij.openapi.ui.MessageType;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeModel;
-import javax.swing.tree.TreePath;
-import javax.swing.tree.TreeSelectionModel;
 
 public class DeleteAction extends AnAction {
     @Override
@@ -22,15 +20,25 @@ public class DeleteAction extends AnAction {
 
         ConfirmDialog dialog = new ConfirmDialog(anActionEvent.getProject(), Bundle.getString("action.DeleteAction.text"),"Confirm deletion of node 'node'",true);
         if (dialog.showAndGet()) {
-            System.out.println("delete");
-//            TreeSelectionModel selectionModel = OperationWindow.tree.getSelectionModel();
-//            OperationWindow.tree.removeSelectionPath(selectionModel.getSelectionPath());
-            DefaultTreeModel model = (DefaultTreeModel)OperationWindow.tree.getModel();
-            TreeSelectionModel selectionModel = OperationWindow.tree.getSelectionModel();
-            System.out.println(model);
+            deleteNode();
         }
 
     }
+
+    private void deleteNode() {
+        DefaultTreeModel model = (DefaultTreeModel)OperationWindow.tree.getModel();
+        Object selectedObject = OperationWindow.tree.getLastSelectedPathComponent();
+        DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) selectedObject;
+        if (selectedNode != null) {
+            if (selectedNode.isRoot ()) {
+                Notifier.notify("the root node cannot be deleted!", MessageType.WARNING);
+            }
+            else {
+                model.removeNodeFromParent (selectedNode); //如果不是根节点，就从父节点中删除
+            }
+        }
+    }
+
     public DeleteAction() {
         super(Bundle.getString("action.DeleteAction.text"), Bundle.getString("action.DeleteAction.description"), AllIcons.General.Remove);
     }
